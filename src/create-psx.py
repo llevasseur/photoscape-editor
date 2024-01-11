6fabf723f548cf21968aa51631ac67d3c288e982
Leevon Levasseur <leevonlevasseur@gmail.com>
2024-01-10 16:12:08 -0800

    Creates lookup file for psx files and sets up user input for create-psx.py

--- src/create-psx.py
+++ src/create-psx.py
@@ -5,6 +5,8 @@ import os
 import sys
 import re
 import zipfile
+import json
+import argparse
 
 DEBUG = 1
 
@@ -14,10 +16,45 @@ def zip_file(file_to_zip, zip_name):
     
 
 def main():
-    zip_file('example.txt', 'example.psxprj', )
+    # Create an ArgumentParser object
+    parser = argparse.ArgumentParser(description='Creates a PSX project of your choice for the Vancouver Canucks game.')
+
+    # Add argument for the choice
+    parser.add_argument('--choice', choices=['game-day', 'final-score', 'box-score'], help='Choose which type of PSX project to create.')
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    # Access the selected choice
+    selected_choice = args.choice
+
+    # Perform actions based on the selected choice
+    if selected_choice == 'game-day':
+        print(f'''
+              You chose game-day.
+        ''')
+    elif selected_choice == 'final-score':
+        print(f'''
+              You chose final-score
+        ''')
+    elif selected_choice == 'box-score':
+        print(f'''
+              You chose box-score
+        ''')
+    else:
+        print(f'''
+        Invalid choice: {selected_choice}. Please choose from the available options:
+              game-day
+              final-score
+              box-score
+        Use the format: python3 create-psx.py --choice game-day
+        ''')
+        return
+
+    #zip_file('example.txt', 'example.psxprj', )
     
-    print('''
-    File example.txt has been zipped to example.zip.
+    print(f'''
+    A new {selected_choice} PSX file has been zipped to games/date/{selected_choice}.psxprj.
     ''')
     
     
