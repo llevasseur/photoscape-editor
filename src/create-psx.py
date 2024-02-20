b7cef0875093a351dec5a4076f65dc3620adf078
Leevon Levasseur <leevonlevasseur@gmail.com>
2024-02-20 15:13:10 -0800

    Handles scores over 7, shootout yet to be tested

--- src/create-psx.py
+++ src/create-psx.py
@@ -38,16 +38,27 @@ def update_text( psxprj, i, text, type ):
         ''' )
     return
 
-def make_shootout_text( stat, index = 0 ):
-    # Bug: Need to x text boxes when line count = 3x
-    # Note: First text box has SHOOTOUT\n
+def make_shootout_text( stat, skip = True, index = 0, initial = True, number_of_spaces = 0 ):
+    # Handles all situations of the shootout. i.e., if there are 
+    # 8 scorers for one team and 7 for the other, it groups them in 3's other than the first one
     text = ''
-    if index == 0: 
-        text += 'SHOOTOUT\n'
     scorers = stat.get( 'SCORERS' )
-    for i in range( index, len( scorers ) ):
-        text += scorers[ i ]
-        if ( i != len( scorers ) - 1 ):
+    if initial: 
+        text += 'SHOOTOUT\n\n'
+        if skip:
+            for i in range( index, len( scorers ) ):
+                text += scorers[ i ]
+                if ( i != len( scorers ) - 1 ):
+                    text += '\n'
+            return text
+        
+        elif len( scorers ) > 0:
+            text += scorers[ index ]
+    else:
+        for i in range( index, min( len( scorers ) - index, index + 3 ) ):
+            text += scorers[ i ]
+            text += '\n'
+        for i in range( 0, number_of_spaces ):
             text += '\n'
 
     return text
@@ -64,9 +75,10 @@ def make_scorer_text( stat, team ):
     # Convert period into correct format
     period = periods.get( stat.get( 'PERIOD' ) )
     
-    # Handle shootout
+    # Handle shootout and know it's only one page.
     if period == 'SHOOTOUT':
-        return make_shootout_text( stat, team )
+        skip = True # just handle it like normal, too messed up thinking about all the possibilities 
+        return make_shootout_text( stat, skip )
     
     # Handle other types of periods
     time = stat.get( 'TIME' )
@@ -287,7 +299,36 @@ def update_psxprj( selected_choice, source, date_file ):
                 # Create CANUCKS and OTHER SCORERS
                 canucks_scorers = stats.get( 'CANUCKS' )
                 other_scorers = stats.get( 'OTHER' )
-                n = max( len( canucks_scorers ) // 7, len( other_scorers ) // 7 ) + 1
+                print(other_scorers, len(other_scorers))
+                
+                # If there is a Shootout section in CANUCKS, determine the length 
+                # of the SCORERS tab for both CANUCKS and OTHER, add 1 and divide 
+                # that by 3 and add 1:
+                # case 1: 2 // 3 = 0 + 1 = 1
+                # case 2: 3 // 3 = 1 + 1 = 2
+                # case 3: 4 // 3 = 1 + 1 = 2
+                # case 4: 5 // 3 = 1 + 1 = 2
+                # case 5: 6 // 3 = 2 + 1 = 3
+                so_canucks = canucks_scorers[ len( canucks_scorers ) - 1 ]
+                so_other = other_scorers[ len( other_scorers ) - 1 ]
+                so = False
+                canucks_so_n = 0
+                other_so_n = 0
+                so_n = 0
+
+                # If Canucks table has SHOOTOUT, we know Other table has SHOOTOUT
+                if  so_canucks.get( 'PERIOD' ) == 'SHOOTOUT':
+                    so = True
+                    canucks_so_n = ( 1 + len( so_canucks.get( 'SCORERS' ) ) ) // 3 + 1
+                    other_so_n = ( 1 + len( so_other.get( 'SCORERS' ) ) ) // 3 + 1 
+                    so_n = max( 
+                         canucks_so_n,  
+                         other_so_n
+                    )
+                    so_n -= 1 # Minus 1 because n calculated below takes into account the SHOOTOUT element in the list of scorers
+                    
+
+                n = max( ( len( canucks_scorers ) + 6 ) // 7, ( len( other_scorers ) + 6 ) // 7 ) + so_n
 
                 # Keep track of total canucks and other scorers counter
                 canucks_i = other_i = 0
@@ -327,23 +368,83 @@ def update_psxprj( selected_choice, source, date_file ):
 
                 # Handle multiple box-scores if necessary
                 if ( n > 1 ):
-                    psx_path = cwd + '/assets/templates/box-score-temp/'
                     
                     # i = 2 because this will make box-score-2, box-score-3, ... box-score-n
                     for i in range( 2, n + 1 ):
+                        # Find template path for selected choice
+                        psx_path = cwd + f'/assets/templates/{ selected_choice }-temp/'
+
+                        # Copy os-dependent psx project into template
+                        copy_file_to_directory( cwd + f'/assets/templates/{ os.sys.platform }/{ selected_choice }/psxproject.json', psx_path )
+
+                        # Create directory for game date and template if it doesn't exist
+                        destination = cwd + f'/json/games/{ date_file }/{ selected_choice }-temp-{i}/'
+                        create_directory( destination )
+
+                        # Copy selected template to new directory
+                        copy_directory( psx_path, destination )
+                        source = cwd + f'/json/games/{ date_file }/{ selected_choice }-temp-{i}'
+
+                        # Find psxprj from copied template source json
+                        with open( source + f'/psxproject.json', 'r' ) as json_file:
+                            psxprj = json.load( json_file )
+                        '''
+                        
+                        # Find template path for selected choice
+                        psx_path = cwd + f'/assets/templates/box-score-temp-{i}/'
+
+                        source = cwd + f'/json/games/{ date_file }/{ selected_choice }-temp-{i}'
+                        # Copy os-dependent psx project into template
+                        copy_file_to_directory( source + f'/psxproject.json', psx_path )
+
+                        # Find psxprj from copied template source json
+                        with open( source + f'/psxproject.json', 'r' ) as json_file:
+                            psxprj = json.load( json_file )
 
                         destination = cwd + f'/json/games/{ date_file }/{ selected_choice }-temp-{ i }/'
                         copy_directory( psx_path, destination )
-                        source = cwd + f'/json/games/{ date_file }/{ selected_choice }-temp-{ i }'
+                        source = cwd + f'/json/games/{ date_file }/{ selected_choice }-temp-{ i }'''
 
                         # Iterate over CANUCKS SCORERS
                         # Restrict box-score to only go to 7
                         start = canucks_i
-                        for j in range( start, min( len( canucks_scorers ) - start, start + 7 ) ):
+                        canucks_so_i = 0
+                        for j in range( start, min( len( canucks_scorers ), start + 7 ) ):
                             # Update CANUCKS SCORER
-                            index = j - start + 3
-                            text = make_scorer_text( canucks_scorers[ canucks_i ], 'CANUCKS' )
-                            update_text( psxprj, index, text, type )
+                            if so and canucks_i == len( canucks_scorers ) - 1:
+                                # FOUND AND NOW HANDLE SHOOTOUT
+                                # canucks_so_n is the number of text boxes required
+                                # If there are not enough boxes left on this page, need to go the next one
+                                # Thats when we need to hit break
+                                # The first one always has SHOOTOUT\n\nScorer
+                                # Problem: canucks_so_n and n change based on if a new page is needed
+                                # Doesn't necessarily mean other_so_n changes, and not even n.
+                                for k in range( j, canucks_so_n - ( j - start ) ):
+                                    index = k - start + 3
+                                    if k == j:
+                                        # Need SHOOTOUT\n\n
+                                        skip = False
+                                        is_initial = True
+                                        text = make_shootout_text( canucks_scorers[ canucks_i ], skip, canucks_so_i, is_initial)
+                                        canucks_so_i += 1
+                                    else:
+                                        # Just list scorers, however we don't want space between at the top.
+                                        # Thus, append x \n's, where x is 4 - ( min( canucks_so_n - canucks_so_i, 3 ) )
+                                        skip = False
+                                        is_initial = False
+                                        number_of_spaces = 4 - ( min( canucks_so_n - canucks_so_i, 3 ) )
+                                        text = make_shootout_text( canucks_scorers[ canucks_i ], skip, canucks_so_i, is_initial, number_of_spaces )
+                                        # set shooter index to match what has been added
+                                        canucks_so_i += min( canucks_so_n - canucks_so_i, 3 )
+
+                                    
+                                # Don't update canucks_i, that way we can hit this again if needed in a new box-score
+                                # Leave the loop
+                                break
+                            else:
+                                index = j - start + 3
+                                text = make_scorer_text( canucks_scorers[ canucks_i ], 'CANUCKS' )
+                                update_text( psxprj, index, text, type )
 
                             # Increment canucks scorers index
                             canucks_i += 1
@@ -351,19 +452,43 @@ def update_psxprj( selected_choice, source, date_file ):
                         # Iterate over OTHER SCORERS
                         # Restrict box-score to only go to 7
                         start = other_i 
-                        for j in range( start, min( len( other_scorers ) - start, start + 7 ) ):
+                        other_so_i = 0
+                        
+                        for j in range( start, min( len( other_scorers ), start + 7 ) ):
                             # Update OTHER SCORER
-                    
-                            index = j - start + 10
-                            text = make_scorer_text( other_scorers[ other_i ], 'OTHER  ' )
-                            update_text( psxprj, index, text, type )
+                            if so and other_i == len( other_scorers ) - 1:
+                                # FOUND AND NOW HANDLE SHOOTOUT
+                                for k in range( j, other_so_n - ( j - start ) ):
+                                    index = k - start + 3
+                                    if k == j:
+                                        # Need SHOOTOUT\n\n
+                                        is_initial = True
+                                        text = make_shootout_text( other_scorers[ other_i ], other_so_i, is_initial)
+                                        other_so_i += 1
+                                    else:
+                                        # Just list scorers, however we don't want space between at the top.
+                                        # Thus, append x \n's, where x is 4 - ( min( other_so_n - other_so_i, 3 ) )
+                                        is_initial = False
+                                        number_of_spaces = 4 - ( min( other_so_n - other_so_i, 3 ) )
+                                        text = make_shootout_text( other_scorers[ other_i ], other_so_i, is_initial, number_of_spaces )
+                                        # set shooter index to match what has been added
+                                        other_so_i += min( other_so_n - other_so_i, 3 )
+
+                                # Don't update other_i, that way we can hit this again if needed in a new box-score
+                                # Leave the loop
+                                break
+                            else:
+                                index = j - start + 10
+                                text = make_scorer_text( other_scorers[ other_i ], 'OTHER  ' )
+                                update_text( psxprj, index, text, type )
 
                             # Increment other scorers index
                             other_i += 1
 
                         # Update the source json
-                        with open( source + '/psxproject.json', 'w' ) as json_file:
+                        with open( source + f'/psxproject.json', 'w' ) as json_file:
                             json.dump( psxprj, json_file, indent=4 )
+                            print(json_file)
                             if ( DEBUG ):
                                 print( f'''
                                 JSON File { source }/psxproject.json : Updated!
